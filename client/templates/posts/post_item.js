9251ee99d2c0643b65adcf348ec2ab586051e5ee
Fabian Widyadri <fabianwidyadri@Fabians-MacBook-Pro.local>
2014-12-09 22:41:59 -0800

    Added remove and edit

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,4 +1,8 @@
 Template.postItem.helpers({
+	ownPost: function(){
+		return this.usedId === Meteor.userId();
+	},
+
 	domain: function(){
 		var a = document.createElement('a');
 		a.href = this.url;
