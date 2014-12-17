e6e8a3a8753c3b9145404c957b46a4878f0f4f76
Fabian Widyadri <fabianwidyadri@Fabians-MacBook-Pro.local>
2014-12-16 23:11:17 -0800

    Add error window

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -5,27 +5,39 @@ Template.postEdit.events({
 		var currentPostId = this._id;
 
 		var postProperties = {
+			_id: this._id,
 			url: $(e.target).find('[name=url]').val(),
 			title: $(e.target).find('[name=title]').val()
 		}
 
-		Post.update(currentPostId, {$set: postProperties}, function(error){
-			if (error) {
-        	// display the error to the user
-				alert(error.reason); 
-			} else {
-        		Router.go('postPage', {_id: currentPostId});
-      		}
-		});
+		Meteor.call('postUpdate', postProperties, function(error, result) { // display the error to the user and abort
+		if (error)
+			return throwError(error.reason);
+      	
+      	// show this result but route anyway
+		if (result.postExists)
+			throwError('This link has already been posted');
+
+      	Router.go('postPage', {_id: result._id});
+    	});
+			 
+		// Post.update(currentPostId, {$set: postProperties}, function(error, result){
+		// 	if (error) {
+  //       		// display the error to the user
+		// 		throwError(error.reason); 
+		// 	} else {
+  //       		Router.go('postPage', {_id: currentPostId});
+  //     		}
+		// });
 	},
 
 	'click .delete': function(e){
 		e.preventDefault();
-
-		if(confirm("Delete this post?")){
-			var currentPostId = this._id;
-			Post.remove(currentPostId);
-			Router.go('postList');
-		}
+    
+	    if (confirm("Delete this post?")) {
+	      var currentPostId = this._id;
+	      Posts.remove(currentPostId);
+	      Router.go('postsList');
+	    }
 	}
 });
